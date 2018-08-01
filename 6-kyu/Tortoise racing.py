a79e9a27771ff65c4eacaceadc0ca6714952f147
411528Andy <41285753+411528Andy@users.noreply.github.com>
2018-08-01 21:50:17 +0800

    Update Tortoise racing.py

--- 6-kyu/Tortoise racing.py
+++ 6-kyu/Tortoise racing.py
@@ -2,4 +2,4 @@ def race(v1, v2, g):
     h = g/(v2-v1)
     m = (g % (v2-v1)) * 60 / (v2-v1)
     s = (((g % (v2-v1)) * 60) % (v2-v1)) * 60 / (v2-v1)
-    return None if v1 >= v2 else [h, m, s]
+    return None if v1 >= v2 else [int(h), int(m), int(s)]
